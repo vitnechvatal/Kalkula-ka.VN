ed56fe9da0b4a54de99c00d6994ba0849fd44a4b
vozojk <vojtech.sandera@gmail.com>
2025-03-26 09:56:32 +0100

    pero

--- Kalkulačka.VN/Form1.Designer.cs
+++ Kalkulačka.VN/Form1.Designer.cs
@@ -29,12 +29,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btPlus = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btRovno = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btPlus
+            // 
+            this.btPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btPlus.Location = new System.Drawing.Point(361, 42);
+            this.btPlus.Name = "btPlus";
+            this.btPlus.Size = new System.Drawing.Size(64, 60);
+            this.btPlus.TabIndex = 0;
+            this.btPlus.Text = "+";
+            this.btPlus.UseVisualStyleBackColor = true;
+            this.btPlus.Click += new System.EventHandler(this.btPlusClick);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplay.Location = new System.Drawing.Point(8, 7);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtDisplay.Size = new System.Drawing.Size(417, 29);
+            this.txtDisplay.TabIndex = 1;
+            this.txtDisplay.Text = "0";
+            // 
+            // btRovno
+            // 
+            this.btRovno.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btRovno.Location = new System.Drawing.Point(361, 422);
+            this.btRovno.Name = "btRovno";
+            this.btRovno.Size = new System.Drawing.Size(64, 60);
+            this.btRovno.TabIndex = 2;
+            this.btRovno.Text = "=";
+            this.btRovno.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(430, 494);
+            this.Controls.Add(this.btRovno);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btPlus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalkulačka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btPlus;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btRovno;
     }
 }
 
--- Kalkulačka.VN/Form1.cs
+++ Kalkulačka.VN/Form1.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace Kalkulačka.VN
 {
+    double dblCislo1, dblCislo2;
+    string strOperace;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            // nastavení proměnných
+            strOperace = "";
+            dblCislo1 = dblCislo2 = 0;
+
+        }
+
+        private void btPlusClick(object sender, EventArgs e)
+        {
+            strOperace = "+";
+
+            dblCislo1 =Convert.ToDouble( txtDisplay.Text);
         }
     }
 }
